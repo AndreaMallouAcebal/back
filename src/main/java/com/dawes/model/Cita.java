acc037c3de927a0227cc805823ad646f529e1026
adrigibsonsg <adri_gibsonsg@hotmail.com>
2023-03-06 20:42:36 +0100

    Security totally added and Improved, now the funcionality let non users to list

--- src/main/java/com/dawes/model/Cita.java
+++ src/main/java/com/dawes/model/Cita.java
@@ -6,14 +6,7 @@ import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, 
 					property  = "id", 
